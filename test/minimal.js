5e6c8e92b7d750a4a3a54e84956a90fb2476b33f
Matt Carter <m@ttcarter.com>
2023-04-01 17:51:43 +1000

    task: Basic proof-of-concept

---
+++ test/minimal.js
@@ -0,0 +1,28 @@
+import {expect} from 'chai';
+import DoopParser from '#lib/parser';
+import {dirName} from '@momsfriendlydevco/es6';
+
+describe('@doop/esm-loader - minimal block test', ()=> {
+
+	it('parse a basic example', ()=> {
+		let testPath = `${dirName('..')}/test/data/minimal.doop`;
+
+		return new DoopParser(testPath)
+			.parse()
+			.then(parser => {
+				expect(parser.blocks).to.be.an('object');
+
+				expect(parser.blocks).to.be.deep.equal({
+					'script0': {
+						id: 'script0',
+						tag: 'script',
+						attrs: {},
+						lineStart: 2,
+						lineEnd: 2,
+						source: "console.log('Hello World');",
+					},
+				});
+			})
+	})
+
+});
---
+++ test/parser.js
@@ -0,0 +1,72 @@
+import {expect} from 'chai';
+import DoopParser from '#lib/parser';
+import {dirName} from '@momsfriendlydevco/es6';
+
+describe('@doop/esm-loader - basic block test', ()=> {
+
+	let parser;
+	let testPath = `${dirName('..')}/test/data/simple.doop`;
+	let emitted = {
+		orphanedLines: [],
+	};
+
+	it('prepare a class instance', ()=>
+		new DoopParser(testPath)
+			.on('orphanedLines', data => emitted.orphanedLines.push(data))
+			.parse()
+			.then(result => parser = result)
+	);
+
+	it('parsed SFC blocks', ()=> {
+		expect(parser.blocks).to.be.an('object');
+
+		expect(parser.blocks).to.have.property('three');
+
+		expect(parser.blocks.three).to.have.property('attrs');
+		expect(parser.blocks.three.attrs).to.be.an('object');
+		expect(parser.blocks.three.attrs).to.deep.equal({
+			id: 'three',
+			foo: 123,
+			bar: true,
+			baz: 'Test String',
+		});
+
+		expect(parser.blocks.three).to.have.property('source');
+		expect(parser.blocks.three.source).to.be.a('string');
+	});
+
+
+	// Not working correctly yet but not essencial for now
+	it.skip('detect orphaned content', ()=> {
+		expect(emitted.orphanedLines).to.be.deep.equal([
+			[
+				'Out of bounds leading whitespace',
+				'',
+			],
+			[
+				'',
+				'Out of bounds mid whitespace',
+				'',
+			],
+			[
+				'',
+				'Out of bounds mid whitespace x1',
+				'Out of bounds mid whitespace x2',
+				'Out of bounds mid whitespace x3',
+				'',
+			],
+			[
+				'',
+				'Out of bounds trailing whitespace',
+			],
+		]);
+
+		expect(parser.index().split(/\n/)).to.be.deep.equal([
+			'/* Generated index file from @doop/esm-loader */',
+			`import script0 from '${testPath}?block=script0'`,
+			`import script1 from '${testPath}?block=script1'`,
+			`import three from '${testPath}?block=three'`,
+		]);
+	});
+
+});
