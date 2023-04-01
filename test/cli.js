fe26949883982473c533858a987ce514b9875ae7
Matt Carter <m@ttcarter.com>
2023-04-01 18:41:10 +1000

    task: Proof of concept for loader

---
+++ test/cli.js
@@ -0,0 +1,21 @@
+import {expect} from 'chai';
+import {dirName} from '@momsfriendlydevco/es6';
+import {execa} from 'execa';
+
+describe('@doop/esm-loader - CLI test', ()=> {
+
+	it('execute a simple Node program', ()=> {
+		let rootPath = dirName('..');
+		let loaderPath = `${rootPath}/lib/index.js`;
+		let testPath = `${rootPath}/test/data/cli.doop`;
+
+		return execa('node', [
+			`--loader=${loaderPath}`,
+			testPath,
+		])
+			.then(({stdout, stderr}) => {
+				console.log('OUT', stdout);
+			})
+	})
+
+});
